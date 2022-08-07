f3c55678a51cca4c444a41b83f3a3eeb3ba2b297
Descartes Sainval <descartes.sainval@gmail.com>
2022-08-07 18:14:55 +0200

    add comb attack1-2 and clean code

--- Cours_n-6_Beats_Them_All/Assets/Script/Enemy2SM.cs
+++ Cours_n-6_Beats_Them_All/Assets/Script/Enemy2SM.cs
@@ -3,111 +3,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemy2SM : MonoBehaviour
 {
     public enum Enemy2Stat
     {
-        IDLE, 
-        WALK, 
+        IDLE,
+        WALK,
         JUMP_DOWN, JUMP_MAX, JUMP_UP,
-        ATTACK1, ATTACK2, 
-        AIR_ATTACK1, AIR_ATTACK2, 
+        ATTACK1, ATTACK2,
+        AIR_ATTACK1, AIR_ATTACK2,
         HURT, //blesser
-        AIR_HURT, 
+        AIR_HURT,
         THROW, //jeter
         DEATH
     }
-    
+
     public Enemy2Stat currentState;
-
     [Header("STATE")]
-     Animator animator;
+    Animator animator;
 
-    [SerializeField]GameObject graphics;
-    
+    [SerializeField] GameObject graphics;
+
 
     [Header("OBJECT")]
     [SerializeField] GameObject player1;
 
-    [Header("SPEED AND RANGE")]
+    [Header("SPEED,RANGE")]
     [SerializeField] float walkSpeed = 2f; //scriptable ?
-    [SerializeField] float range = 3f;
-
+    [SerializeField] float jumpSpeed = 5f; //scriptable ?
+    [SerializeField] float scope = 3f;
 
     bool isGrounded; // scriptable ?
     bool isJump;
+    bool isAttack = false;
     bool isWalk = false;
-
-
+    bool isOnCollision = false;
+ 
+    float distancePlayerToEnemy;
 
     Rigidbody2D rb2D;
-    Vector2 dirMove; 
+    Vector2 dirMove;
     Vector2 dirMoveNormalized;
-    float distancePlayerToEnemy;
+    Vector2 currentDestination;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        
+
         animator = graphics.GetComponent<Animator>();
-        
+
         currentState = Enemy2Stat.IDLE;
+
+        currentDestination = player1.transform.position;
 
         OnStateEnter();
     }
 
-
-    void OnStateEnter()
-    {
-        switch (currentState)
-        {
-            case Enemy2Stat.IDLE:
-            
-                rb2D.velocity = Vector2.zero;
-                break;
-
-            case Enemy2Stat.WALK:
-               
-                animator.SetBool("WALK", true);
-                break;
-
-            case Enemy2Stat.JUMP_DOWN:
-                //ref sauter bas
-                
-                /*
-                 
-                 */
-                break;
-            case Enemy2Stat.JUMP_MAX:
-                //ref sauter maximum
-                break;
-            case Enemy2Stat.JUMP_UP:
-                //ref sauter basiq ?
-                break;
-            case Enemy2Stat.ATTACK1:
-                break;
-            case Enemy2Stat.ATTACK2:
-                break;
-            case Enemy2Stat.AIR_ATTACK1:
-                break;
-            case Enemy2Stat.AIR_ATTACK2:
-                break;
-            case Enemy2Stat.HURT:
-                //
-                break;
-            case Enemy2Stat.AIR_HURT:
-                break;
-            case Enemy2Stat.THROW:
-                //jeter
-                break;
-            case Enemy2Stat.DEATH:
-                break;
-            default:
-                break;
-        }
-    }
-
-    // Update is called once per frame
     void Update()
     {
 
@@ -117,14 +69,13 @@ public class Enemy2SM : MonoBehaviour
 
         if (rb2D.velocity.x < 0)
         {
-            transform.eulerAngles = new Vector2(0,-180);
+            transform.eulerAngles = new Vector2(0, -180);
         }
 
-        //pas besoin
-        //if (rb2D.velocity.x > 0)
-        //{
-        //    transform.eulerAngles = new Vector2(0, 180);
-        //}
+        if (rb2D.velocity.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
 
         OnStateUpdate();
     }
@@ -144,23 +95,23 @@ public class Enemy2SM : MonoBehaviour
                 break;
             case Enemy2Stat.WALK:
 
-                if (distancePlayerToEnemy <= range)
+                if (distancePlayerToEnemy <= scope)
                 {
                     rb2D.velocity = dirMoveNormalized * walkSpeed;
                 }
-                break;
-            case Enemy2Stat.JUMP_DOWN:
-                
-                break;
-            case Enemy2Stat.JUMP_MAX:
-                break;
-            case Enemy2Stat.JUMP_UP:
                 break;
             case Enemy2Stat.ATTACK1:
                 //collision
                 break;
             case Enemy2Stat.ATTACK2:
                 //collision
+                break;
+            case Enemy2Stat.JUMP_DOWN:
+
+                break;
+            case Enemy2Stat.JUMP_MAX:
+                break;
+            case Enemy2Stat.JUMP_UP:
                 break;
             case Enemy2Stat.AIR_ATTACK1:
                 break;
@@ -171,6 +122,53 @@ public class Enemy2SM : MonoBehaviour
             case Enemy2Stat.AIR_HURT:
                 break;
             case Enemy2Stat.THROW:
+                break;
+            case Enemy2Stat.DEATH:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnStateEnter()
+    {
+        switch (currentState)
+        {
+            case Enemy2Stat.IDLE:
+
+                rb2D.velocity = Vector2.zero;
+                break;
+
+            case Enemy2Stat.WALK:
+                animator.SetBool("WALK", true);
+                break;
+
+            case Enemy2Stat.ATTACK1:
+                isAttack = true;
+                animator.SetBool("ATTACK1", true);
+                break;
+            case Enemy2Stat.ATTACK2:
+                break;
+            case Enemy2Stat.AIR_ATTACK1:
+                break;
+            case Enemy2Stat.AIR_ATTACK2:
+            case Enemy2Stat.JUMP_DOWN:
+                //ref sauter bas
+                break;
+            case Enemy2Stat.JUMP_MAX:
+                //ref sauter maximum
+                break;
+            case Enemy2Stat.JUMP_UP:
+                //ref sauter basiq ?
+                break;
+                break;
+            case Enemy2Stat.HURT:
+                //
+                break;
+            case Enemy2Stat.AIR_HURT:
+                break;
+            case Enemy2Stat.THROW:
+                //jeter
                 break;
             case Enemy2Stat.DEATH:
                 break;
@@ -184,17 +182,20 @@ public class Enemy2SM : MonoBehaviour
         switch (currentState)
         {
             case Enemy2Stat.IDLE:
-                if (distancePlayerToEnemy <= range) // dirMagnitude != 0
+                if (distancePlayerToEnemy <= scope) // dirMagnitude != 0
                 {
                     TransitionToState(Enemy2Stat.WALK);
                 }
-
-                //jump, Attack => ceux sont plutot des action
                 break;
             case Enemy2Stat.WALK:
-                if (distancePlayerToEnemy > range) // dirMagnitude -= 0
+                if (distancePlayerToEnemy > scope) // dirMagnitude -= 0
                 {
                     TransitionToState(Enemy2Stat.IDLE);
+                }
+
+                if (distancePlayerToEnemy <= scope && isAttack == true)
+                {
+                    TransitionToState(Enemy2Stat.ATTACK1);
                 }
                 break;
             case Enemy2Stat.JUMP_DOWN:
@@ -204,6 +205,15 @@ public class Enemy2SM : MonoBehaviour
             case Enemy2Stat.JUMP_UP:
                 break;
             case Enemy2Stat.ATTACK1:
+                if (distancePlayerToEnemy <= scope/*portée*/ )
+                {
+                    TransitionToState(Enemy2Stat.WALK);
+                }
+
+                if (distancePlayerToEnemy > scope)
+                {
+                    TransitionToState(Enemy2Stat.IDLE);
+                }
                 break;
             case Enemy2Stat.ATTACK2:
                 break;
@@ -223,6 +233,25 @@ public class Enemy2SM : MonoBehaviour
                 break;
         }
     }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if ((collision.collider.tag == "Player"))
+        {
+            switch (currentState)
+            {
+                case Enemy2Stat.IDLE:
+                    break;
+                case Enemy2Stat.WALK:
+                    isAttack = true;
+                    break;
+                case Enemy2Stat.ATTACK1:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
 
     void OnStateExit()
     {
@@ -240,6 +269,8 @@ public class Enemy2SM : MonoBehaviour
             case Enemy2Stat.JUMP_UP:
                 break;
             case Enemy2Stat.ATTACK1:
+                isAttack = false;
+                animator.SetBool("ATTACK1", false); 
                 break;
             case Enemy2Stat.ATTACK2:
                 break;
@@ -270,11 +301,8 @@ public class Enemy2SM : MonoBehaviour
         OnStateEnter();
     }
 
-    //lorsque l'object E rentre en collision 
-    //    si collision.gameObject.tag=="player"
-    //        switch
-    //            currentState
 
+    
 
 
 }
